76648df09f91f16af26e6619d46764303535e6c6
Falk Sippach <falk@jug-da.de>
2024-09-03 15:51:57 +0200

    Übung TDD BowlingGame Schritt 1

---
+++ Java/BowlingGameKata/src/main/java/ccsc/bowling/kata/Game.java
@@ -0,0 +1,10 @@
+package ccsc.bowling.kata;
+
+public class Game {
+    public int getScore() {
+        return 0;
+    }
+
+    public void roll(int numberOfPins) {
+    }
+}
---
+++ Java/BowlingGameKata/src/test/java/ccsc/bowling/kata/GameTests.java
@@ -0,0 +1,26 @@
+package ccsc.bowling.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * TODOs
+ *
+ * 1. Test: can roll gutter (no pins at all) ✅
+ * 2. Test: can roll One's 20 times
+ * 3. Test: Spare
+ * 4. Test: Strike
+ * 5. Test: Perfect Match
+ */
+public class GameTests {
+    @Test
+    void canRollGutter() {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.getScore());
+    }
+}
